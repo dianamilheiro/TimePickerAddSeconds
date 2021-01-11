f1eeba34922a82d12fecdb5c3d2d9b2bc40a5bb6
dianamilheiro <57118189+dianamilheiro@users.noreply.github.com>
2021-01-11 12:37:11 +0000

    Add files via upload

--- src/android/TimePicker.java
+++ src/android/TimePicker.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.ikovac.timepickerwithseconds.view;
+package org.apache.cordova.timepickerwithseconds;
 
 import java.text.DateFormatSymbols;
 import java.util.Calendar;
--- src/android/TimePickerWithSeconds.java
+++ src/android/TimePickerWithSeconds.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.ikovac.timepickerwithseconds;
+package org.apache.cordova.timepickerwithseconds;
 
 import java.util.Calendar;
 
